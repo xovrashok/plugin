f6d2c373d4b4021cdcb988091de1430adbbd982b
xovrasok <yuliaruda033@gmail.com>
2023-07-26 18:49:51 +0300

    add 2captcha

---
+++ background.js
@@ -0,0 +1,40 @@
+const scriptTag = document.createElement("script");
+document.head.appendChild(scriptTag);
+
+scriptTag.onload = function () {
+  const apiKey = "0dc13a497a3ee3852095213deb19437a";
+  const solver = new Captcha.Solver(apiKey);
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "solveCaptcha") {
+      const siteKey = message.siteKey;
+      const pageUrl = message.pageUrl;
+
+      solver
+        .recaptcha(siteKey, pageUrl)
+        .then((response) => {
+          console.log("Response from 2captcha:", response);
+          if (response.success) {
+            const captchaSolution = response.solution.gRecaptchaResponse;
+            console.log("Captcha solution:", captchaSolution);
+            sendResponse({ success: true, solution: captchaSolution });
+          } else {
+            console.error("Captcha solving failed:", response.error);
+            sendResponse({
+              success: false,
+              message: "Captcha solving failed.",
+            });
+          }
+        })
+        .catch((error) => {
+          console.error("Error while solving captcha:", error.message);
+          sendResponse({
+            success: false,
+            message: "Error while solving captcha.",
+          });
+        });
+
+      return true;
+    }
+  });
+};
--- content_script.js
+++ content_script.js
@@ -1,86 +1,53 @@
+const pageUrl = window.location.href;
 
-function solveCaptcha(imageData) {
-    const apiKey = '';
-    const serviceEndpoint = 'https://2captcha.com/in.php';
-
-    fetch(serviceEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            apiKey: apiKey,
-            image: imageData,
-        }),
-    })
-        .then((response) => response.json())
-        .then((result) => {
-            if (result.success) {
-                const captchaSolution = result.solution;
-                console.log('Captcha solution:', captchaSolution);
-            } else {
-                console.error('Captcha solving failed:', result.error);
-            }
-        })
-        .catch((error) => {
-            console.error('Error while solving captcha:', error);
-        });
+function captureReCaptcha() {
+    const captchaElement = document.querySelector('body > div.g-recaptcha');
+    if (captchaElement) {
+        const siteKey = captchaElement.getAttribute('data-sitekey');
+        if (siteKey) {
+            chrome.runtime.sendMessage(
+                { type: 'solveCaptcha', siteKey: siteKey, pageUrl: pageUrl },
+                (response) => {
+                    console.log('Response from background.js:', response);
+                    if (response.success) {
+                      const captchaSolution = response.solution;
+                      console.log("Captcha solution:", captchaSolution);
+                    } else {
+                      console.error("Captcha solving failed:", response);
+                    }
+                }
+            );
+        } else {
+            console.error('Failed to capture ReCaptcha site key.');
+        }
+    } else {
+        console.error('ReCaptcha element not found.');
+    }
 }
 
-const injectedScript =
-    '(' +
-    function () {
-        console.log('Script Injected');
+setTimeout(captureReCaptcha, 5000);
 
-        window.solveCaptcha = function (imageData) {
-            const apiKey = '';
-            const serviceEndpoint = 'https://2captcha.com/in.php';
 
-            fetch(serviceEndpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    apiKey: apiKey,
-                    image: imageData,
-                }),
-            })
-                .then((response) => response.json())
-                .then((result) => {
-                    if (result.success) {
-                        const captchaSolution = result.solution;
-                        console.log('Captcha solution:', captchaSolution);
-                    } else {
-                        console.error('Captcha solving failed:', result.error);
-                    }
-                })
-                .catch((error) => {
-                    console.error('Error while solving captcha:', error);
-                });
-        };
-
+const injectedScript ="(" +
+    function() {
+        console.log("Script Injected");
         const monkeyPatch = () => {
             let oldXHROpen = window.XMLHttpRequest.prototype.open;
-            window.XMLHttpRequest.prototype.open = function () {
-                this.addEventListener('load', function () {
+            window.XMLHttpRequest.prototype.open = function() {
+                this.addEventListener("load", function() {
                     const responseBody = this.responseText;
                     console.log(`Response Body: ${responseBody}`);
-                    window.solveCaptcha(responseBody);
                 });
                 return oldXHROpen.apply(this, arguments);
             };
         };
         monkeyPatch();
-    } +
-    ')();';
-
+    } + ")();";
 const injectScript = () => {
-    console.log('Injecting Script');
-    var script = document.createElement('script');
+    console.log("Injecting Script");
+    var script = document.createElement("script");
     script.textContent = injectedScript;
     (document.head || document.documentElement).appendChild(script);
     script.remove();
 };
-
 injectScript();
